41af0440563ce86ff51ad303bdbaa10b63ad5ef9
Jon Kiparsky <jon@kiparsky.net>
2021-03-21 22:19:22 -0400

    Add test for max_stress

--- tests/test_rhymes.py
+++ tests/test_rhymes.py
@@ -1,7 +1,14 @@
-from rhymes import read_cmu_dict
+from rhymes import (
+    max_stress,
+    read_cmu_dict,
+)
 
 class TestReadDictionary:
     def test_read_good_dictionary(self):
         words, phonemes = read_cmu_dict("tests/test_data/fake_dictionary.txt")
         assert "cat" in words
         
+class TestMaxStress:
+    def test_max_stress(self):
+        phonemes = "B IH0 G AE1 T".split(" ")
+        assert max_stress(phonemes) == 3
